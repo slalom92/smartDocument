2903a658f3595e42de2da4bca629a12d260d6e20
slalom92 <slalom92@chunjae.co.kr>
2024-02-15 20:04:27 +0900

    0215

--- js/common.js
+++ js/common.js
@@ -19,9 +19,58 @@ const init = {
             }
             return false;
         });
-    }
+    },
+    contextClickdIdx : null,
+    rightClickable : function(e){
+        $('.rightClickable').on('contextmenu',function(e){
+            e.preventDefault();
+            let x = e.pageX + 'px';            
+            let y = e.pageY + 'px';
+            let clickedElement = $(e.target).parentsUntil('.rightClickable').parent().index();//선택 element index값
+            console.log(clickedElement);
+            $('.contextBox').css({
+                'top':y,
+                'left':x
+            });
+            $('.contextBox').show();
+            init.contextClickdIdx = clickedElement;            
+        });
+    },
+    closeContextmenu : function(e){
+        $(document).bind('click',function(e){
+            let clickedobj = $(e.target);
+            if(!clickedobj.parents().hasClass('contextBox')){
+                $('.contextBox').hide();
+            }
+            return false;
+        });
+        $('.contextBox a').bind('click',function(){
+            $('.contextBox').hide();
+            return false;
+        });
+    },
+    changeName : function(){
+        $('.changeName').bind('click',function(){
+            let clickedValue = $('.rightClickable').eq(init.contextClickdIdx).find('.folderName').text();
+            $('.rightClickable').eq(init.contextClickdIdx).find('.folderName').html('<input type="text" value="'+clickedValue+'" class="changeInput"/>');
+            return false;
+        });
+        $(document).on('blur','.changeInput',function(){
+            let thisVal = $(this).val();
+            $('.rightClickable').eq(init.contextClickdIdx).find('.folderName').html(thisVal);
+        });
+    },
+    openLayer : function(id){
+        $('#'+id).show();
+    },
+    closeLayer : function(id){
+        $('#'+id).hide();
+    },
 }
 
 $(document).ready(function(){
     init.lnbFn();
+    init.rightClickable();
+    init.closeContextmenu();
+    init.changeName();
 });
